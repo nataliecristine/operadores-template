edf04bbc348b658d95a4b92fba854c1b6ef5e26b
Natalie Cristine <ei.nataliecristine@gmail.com>
2023-11-09 22:26:06 -0300

    exercicios aula operadores - exercicios 1 e 2

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,12 @@
-//CODIGO AQUI
+
+//pratica 1
+
+const valorProduto = Number (prompt("valor do produto")
+const percentualDesc = Number (prompt("valor percentual")
+
+const valorDoDesconto = (valorProduto*percentualDesc)
+                        (100 = 10) / 100
+
+console.log("valor do desconto é de R$ " , valorDoDesconto); // 10
+
+console.log("o valor final do produto com desconto é R$" , valorFinal);
